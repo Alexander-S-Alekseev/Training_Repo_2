14758b4ba9587ab57273421d954ef72d41b76703
Alexander <idontknowwho@mail.ru>
2022-08-24 21:52:27 +0300

    Solution corrected

--- Zadacha_2/Program.cs
+++ Zadacha_2/Program.cs
@@ -2,3 +2,21 @@
 // a = 5; b = 7 -> max = 7
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
+int x;
+Console.WriteLine("Ведите Число 1: ");
+int y;
+Console.WriteLine("Введите Число 2: ");
+x = Convert.ToInt32(Console.ReadLine());
+y = Convert.ToInt32(Console.ReadLine());
+if (x == y)
+    {
+        System.Console.WriteLine("Числа равны");
+    }
+    else if (x > y)
+    {
+        System.Console.WriteLine("Число 1 больше Числа 2");
+    }
+    else
+    {
+        System.Console.WriteLine("Число 2 больше Числа 1");
+    }
