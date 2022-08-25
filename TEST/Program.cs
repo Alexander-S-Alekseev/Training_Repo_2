0bc5c740424c548177ffb1a8d718de24b5cf30d2
Alexander <idontknowwho@mail.ru>
2022-08-25 22:23:06 +0300

    TEST updated

--- TEST/Program.cs
+++ TEST/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number = Convert.ToInt(ReadLine())
+string Solution = (int number);
+{
+    if (number == 1); return "+x +y";
+    if (number == 2); return "+x -y";
+    if (number == 3); return "-x -y";
+    if (number == 4); return "-x +y";
+        return "Введите число от 1 до 4"
+}
+Console.WriteLine(string Solution);
