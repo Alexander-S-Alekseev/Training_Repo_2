36b2ed3147eda704aaf7e4248259a3420a232afe
Alexander <idontknowwho@mail.ru>
2022-09-01 17:40:13 +0300

    Problem solved

--- TEST/Program.cs
+++ TEST/Program.cs
@@ -1,10 +1,20 @@
-﻿int number = Convert.ToInt(ReadLine())
-string Solution = (int number);
+﻿int x1 = Coordinate("x", "A");
+int y1 = Coordinate("y", "A");
+int z1 = Coordinate("z", "A");
+int x2 = Coordinate("x", "B");
+int y2 = Coordinate("y", "B");
+int z2 = Coordinate("z", "B");
+
+int Coordinate(string Coord, string Point)
 {
-    if (number == 1); return "+x +y";
-    if (number == 2); return "+x -y";
-    if (number == 3); return "-x -y";
-    if (number == 4); return "-x +y";
-        return "Введите число от 1 до 4"
+    Console.Write($"Введите координату {Coord} точки {Point}: ");
+    return Convert.ToInt32(Console.ReadLine());
 }
-Console.WriteLine(string Solution);
+
+double Solution(double x1, double x2, double y1, double y2, double z1, double z2)
+{
+  return Math.Sqrt(Math.Pow((x2-x1), 2) + Math.Pow((y2-y1), 2) + Math.Pow((z2-z1), 2));
+}
+double Distance =  Math.Round (Solution(x1, x2, y1, y2, z1, z2), 2 );
+
+Console.WriteLine($"Расстояние между точками: {Distance}");
