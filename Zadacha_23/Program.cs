eaf5015a488aa47680e4015cbb46e24301562892
Alexander <idontknowwho@mail.ru>
2022-09-01 19:29:43 +0300

    Problem half-solved

--- Zadacha_23/Program.cs
+++ Zadacha_23/Program.cs
@@ -2,8 +2,20 @@
 // Напишите программу, которая принимает на вход число (N) и 
 // выдаёт таблицу кубов чисел от 1 до N.
 // 3 -> 1, 8, 27
-
-
+int N;
+Console.Write("Введите произвольное число: ");
+N = Convert.ToInt32(Console.ReadLine());
+Cube(N);
+void Cube(int N)
+{
+    int Index = 1;
+    int Cube = 0;
+    {
+    for(Index = 1; Index <= N; Index++);
+    Cube = Math.Pow(Index, 3);
+    Console.Write(Cube + " ");
+    }
+}
 
 
 
