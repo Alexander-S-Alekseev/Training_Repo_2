b067ed7efa383efafdbf69aa0cdecfbaf8f19fd7
Alexander <idontknowwho@mail.ru>
2022-09-01 15:15:32 +0300

    Problem solved

--- Zadacha_19/Program.cs
+++ Zadacha_19/Program.cs
@@ -4,3 +4,16 @@
 // 14212 -> нет
 // 12821 -> да
 // 23432 -> да
+string number;
+Console.Write("Введите пятизначное число: ");
+number = Console.ReadLine();
+{
+    if((number[0] == number[4]) && (number[1] == number[3]))
+        {    
+            Console.Write("Введенное число является полиндромом");
+        }
+    else
+        {
+            Console.Write("Введенное число не является полиндромом");
+        }
+}
