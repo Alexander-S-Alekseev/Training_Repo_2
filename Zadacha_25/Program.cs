f97450fe08a3f80b26519110330a7c2f6beb95aa
Alexander <idontknowwho@mail.ru>
2022-09-04 14:37:35 +0300

    Problem solved

--- Zadacha_25/Program.cs
+++ Zadacha_25/Program.cs
@@ -2,5 +2,10 @@
 // возводит число A в натуральную степень B.
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
-
-
+Console.WriteLine("Введите произвольное число 1: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите произвольное число 2: ");
+int y = Convert.ToInt32(Console.ReadLine());
+{
+    Console.WriteLine(Math.Pow( x, y));
+}
