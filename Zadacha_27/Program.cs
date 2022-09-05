7917b06b67238ebf1bdc457302b843ba262cf6be
Alexander <idontknowwho@mail.ru>
2022-09-05 18:56:36 +0300

    Problem not solved

--- Zadacha_27/Program.cs
+++ Zadacha_27/Program.cs
@@ -3,4 +3,16 @@
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+Console.Write("Введите произвольное число: ");
+string input = Console.ReadLine();
+Console.WriteLine(Sum(input));
+int Sum(string input);
+{
+    int result = 0;
+    for (int i = 0; i < input.Length; i++);
+    {
+        result += result[i] - '0';
+    }
+    
+}
 
