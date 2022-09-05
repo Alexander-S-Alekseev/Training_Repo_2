fc3e53a1905256ac9c5eae10f51a1c9407bd86cc
Alexander <idontknowwho@mail.ru>
2022-09-05 18:54:05 +0300

    Problem not solved

--- Zadacha_29/Program.cs
+++ Zadacha_29/Program.cs
@@ -2,5 +2,29 @@
 // выводит их на экран.
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while(index < length)
+    {
+        collection[index] = new Random().Next(1, 100);
+        index++;
+    }
+}
+
+void PrintArray (int[] col);
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
 int [] array = new int [10];
-Console.Write(array);
+
+FillArray(array);
+PrintArray(array);
