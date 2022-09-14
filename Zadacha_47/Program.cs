522f79766a9f61da17ade125b5118bb2308723b8
Alexander <idontknowwho@mail.ru>
2022-09-14 19:48:54 +0300

    Problem solved

--- Zadacha_47/Program.cs
+++ Zadacha_47/Program.cs
@@ -4,3 +4,33 @@
 // 0,5 7 -2 -0,2
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
+Console.WriteLine("Введите число строк: ");
+int Lines = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число столбцов: ");
+int Columns = Convert.ToInt32(Console.ReadLine());
+double[,] Array = new double[Lines, Columns];
+FillArray (Array);
+PrintArray (Array);
+
+void FillArray(double[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+            arr[i, j] = Convert.ToDouble(new Random().Next(-100, 100));
+    }
+}
+
+void PrintArray(double[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write (arr[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
